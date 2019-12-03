c180f401741f9efa14801b4a944750b2667a0708
Chris DeSilva <chris.a.desilva@gmail.com>
2019-12-03 15:05:29 -0600

    Remove Q from beginning of questions

--- src/components/faqtemplate.js
+++ src/components/faqtemplate.js
@@ -7,7 +7,7 @@ const FAQTemplate = props => {
 
     return (
         <div className="px-4">
-            <h2 className={isOpen ? "cursor-pointer text-base font-bold" : "cursor-pointer text-base"} onClick={() => toggleIsOpen(!isOpen)}>Q: {props.question}</h2>
+            <h2 className={isOpen ? "cursor-pointer text-base font-bold" : "cursor-pointer text-base"} onClick={() => toggleIsOpen(!isOpen)}>{props.question}</h2>
             <Collapse isOpened={isOpen}>
                 <p className="mb-0 pb-4">{props.answer}</p>
             </Collapse>
